7f226a3d20b5e22687a0f5a2dab641901d3cbbda
Lamine Ndiaye <lamine.ndy@gmail.com>
2017-06-10 20:43:15 +0200

    add swift lint + playerviewcontroller

---
+++ LiveStream/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LiveStream
+//
+//  Created by Lamine Ndiaye on 10/06/2017.
+//
+//
+
+import Foundation
+
+enum Constants {
+    static let liveStreamUrlString = "https://wowza-cloudfront.streamroot.io/liveorigin/stream4/playlist.m3u8"
+}
